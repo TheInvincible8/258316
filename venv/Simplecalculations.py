385f97d779ab48a1a7dca8fb2a50a3822f0707cd
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-18 12:29:12 +0530

    Simple calculations in python

---
+++ venv/Simplecalculations.py
@@ -0,0 +1,11 @@
+print (2+4+5)
+print (2*5/3)
+print(12/3)
+
+#__problem statement for simple calculations--
+#Today is a holiday at the children's camp, so all the children will be served ice cream.
+#There are 3 groups of children: 23 children in the first group, 27 in the second, and 18 in the third.
+# Every child should get 2 scoops of ice cream.
+#Write a program to calculate and output the total number of ice cream scoops we need.
+#Solution
+print(23*2+27*2+18*2)
