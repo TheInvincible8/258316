3de6f6b685112fae7dd6e5b72a9dc318bcc1a2e6
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-18 21:07:50 +0530

    variable rules

---
+++ Variables.py
@@ -0,0 +1,21 @@
+user='john'
+print(user)
+
+#The only characters that are allowed are letters, numbers, and underscores. Also, they can't start with numbers.
+this_is_correct=3
+print(this_is_correct)
+
+#Python is a case sensitive programming language.
+# Thus, Lastname and lastname are two different variable names in Python.
+x=4
+print(x)
+print(x+3)
+print(x)
+
+
+spam="eggs"
+print(spam*3)
+y=123.45
+print(y)
+y="A string"
+print(y)
