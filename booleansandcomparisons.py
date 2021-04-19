8e63391dc00587a1b26e8ec67218d9c596f7a4e7
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-20 00:04:57 +0530

    Booleans and comparison implementation

---
+++ booleansandcomparisons.py
@@ -0,0 +1,24 @@
+my_boolean = True
+print(my_boolean)
+
+print(2 == 3)
+
+print("hello" == "hello")
+
+#Another comparison operator, the not equal operator (!=), evaluates to True if the items being compared aren't equal, and False if they are.
+print(1 != 1)
+
+print("eleven" != "seven")
+
+print(2 != 10)
+
+print(7 > 5)
+
+print(10 < 10)
+
+print(7 <= 8)
+
+print(9 >= 9.0)
+#Greater than and smaller than operators can also be used to compare strings lexicographically (the alphabetical order of words is based on the alphabetical order of their component letters).
+print("annie">"annja")
+print( 8.7 <= 8.70 )
