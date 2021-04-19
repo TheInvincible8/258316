0d5e9a4b60d4ec536f51774bac80aa58a808fdc4
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-19 23:52:29 +0530

    inputs

---
+++ input.py
@@ -0,0 +1,29 @@
+#
+
+#Let's assume we want to take the age of the user as input.
+#We know that the input() function returns a string.
+#To convert it to a number, we can use the int() function:
+
+age=int(input())
+print(age)
+
+age1=42
+print("The age is:"+str(age1))
+
+#Simple Calculator
+
+
+#Write a program to take two integers as input and output their sum.
+
+#Sample Input:
+#2
+8
+
+#Sample Output:
+#10
+
+x=input()
+y=input()
+res=int(x)+int(y)
+print(res)
+
