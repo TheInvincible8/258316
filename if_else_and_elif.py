0ca8e6838ca48e6c13ab083ca4b4aa361c39c385
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-21 14:30:54 +0530

    228316 if , else and elif implementation

---
+++ if_else_and_elif.py
@@ -0,0 +1,37 @@
+if 10>5:
+    print('10 is greater then 5')
+print("program ended")
+
+#nested
+num = 12
+if num > 5:
+    print("Bigger than 5")
+    if num <=47:
+        print("Between 5 and 47")
+num = 7
+if num > 3:
+   print("3")
+   if num < 5:
+      print("5")
+      if num ==7:
+         print("7")
+#else
+
+x = 4
+if x == 5:
+    print("Yes")
+else:
+    print("No")
+
+#elif
+num = 3
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Three")
+else:
+    print("Something else")
+
+
