e79990f51b8387a76fbdb0089f2c114d2c4ffe58
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-26 09:48:38 +0530

    258316 if , boolean logic implementation

---
+++ Boolean_Logic.py
@@ -0,0 +1,16 @@
+#boolean and
+print(1==1 and 2==2)
+print(2>1 and 1>2)
+
+if (1 == 1) and (2 + 2 > 3):
+  print("true")
+else:
+  print("false")
+
+#boolean or
+print(1==1 or 2==3)
+print(2<1 or 1>3)
+
+#boolean not
+print(not 1==1)
+print(not 1>7 )
