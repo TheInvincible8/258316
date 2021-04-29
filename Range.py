a954d4e6ff70894732be1782124c65e9566d1cb9
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-30 03:39:19 +0530

    range function

---
+++ Range.py
@@ -0,0 +1,25 @@
+num=list(range(10))
+print(num)
+numbers=list(range(3,10))
+print(numbers)
+
+#########################
+print(range(20)==range(0,20))
+
+##########
+nums = list(range(5, 8))
+print(len(nums))
+
+#range can have a third argument,
+# which determines the interval of the sequence produced, also called the step.
+
+num1=list(range(3,8,2))
+print(num1)
+
+#########
+for i in range(6):
+    print("pankaj")
+######
+
+for i in range(0,20,2):
+    print(i)
