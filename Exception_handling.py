70eb686d262f258df2fbad57be040d65b9af332f
Pankaj Dadwal <pt8699469948@gmail.com>
2021-04-30 05:22:45 +0530

    Exceptions implementation

---
+++ Exception_handling.py
@@ -0,0 +1,26 @@
+try:
+    variable = 10
+    print(variable + "hello")
+    print(variable / 2)
+except ZeroDivisionError:
+    print("Divided by zero")
+except (ValueError, TypeError):
+    print("Error occurred")
+
+#finally
+try:
+    print("Hello")
+    print(1 / 0)
+except ZeroDivisionError:
+    print("Divided by zero")
+finally:
+    print("This code will run no matter what")
+
+#Raising Exceptions
+
+print(1)
+raise ArithmeticError
+print(2)
+
+#Assertions
+
